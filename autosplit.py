e946f8512aee88b12b4bd357b0f32bda54b270f9
chelast55 <tim@amoto.de>
2021-06-23 18:20:27 +0200

    Complete switch to config file

--- autosplit.py
+++ autosplit.py
@@ -6,29 +6,17 @@ from PIL import ImageGrab
 import cv2
 import time
 
-# Do setup at start?
-setup_at_start = True  # TODO: impement that user inpus are saved in and loaded from config file
-
-# For key codes see https://pynput.readthedocs.io/en/latest/keyboard.html#pynput.keyboard.Key
-split_key = Key.cmd
-
-# Corners of stream preview window
-video_preview_coords = [0, 0, 100, 100]
-
-# Blackscreen count values at which to split # TODO: move to splits.txt
-splits = [1, 3, 5]
-
-# Threshold for average gray value for a screen to count as blackscreen (0 by default) # TODO: move to config.txt
-blackscreen_threshold = 0
-
-# Times per second a capture is taken (NOTE: this is a maximum and possibly unreachable) # TODO: move to config.txt
-max_capture_rate = 60
-
-# Delay to prevent multiple splits per blackscreen in seconds # TODO: move to config.txt
-after_split_delay = 10
+setup_at_start: bool         # Do setup at start?
+split_key: Key               # For key codes see https://pynput.readthedocs.io/en/latest/keyboard.html#pynput.keyboard.Key
+video_preview_coords = []    # Corners of stream preview window
+splits = []                  # Blackscreen count values at which to split
+blackscreen_threshold: float # Threshold for average gray value for a screen to count as blackscreen (0 by default)
+max_capture_rate: int        # Times per second a capture is taken (NOTE: this is a maximum and possibly unreachable)
+after_split_delay: float     # Delay to prevent multiple splits per blackscreen in seconds
 
 
 if __name__ == '__main__':
+
     mouse = MouseController()
     keyboard = KeyboardController()
     preview_coord_counter = 0
@@ -54,6 +42,12 @@ if __name__ == '__main__':
         elif video_preview_coords[3] == video_preview_coords[1]:
             video_preview_coords[3] += 1
 
+    def on_press_split_key(key):
+        global split_key
+        split_key = key
+        print(repr(key) + " was set as your Split key!")
+        return False
+
     def on_click_set_coords(x, y, button, pressed):
         global video_preview_coords
         global preview_coord_counter
@@ -62,32 +56,84 @@ if __name__ == '__main__':
                 video_preview_coords[0] = x
                 video_preview_coords[1] = y
                 preview_coord_counter += 1
-                print("First Corner set to (" + str(x) + "," + str(y) + ")")
+                print("First Corner was set to (" + str(x) + "," + str(y) + ")!")
             else:
                 video_preview_coords[2] = x
                 video_preview_coords[3] = y
                 preview_coord_counter += 1
-                print("Second Corner set to (" + str(x) + "," + str(y) + ")")
+                print("Second Corner was uset to (" + str(x) + "," + str(y) + ")!")
                 correct_coords()
                 return False
+
+    # Read config
+    with open("config.cfg", 'r') as config_file:
+        settings = config_file.readlines()
+        setup_at_start = eval(settings[0])
+        # non-alphanumeric keys need to be handled differently
+        if settings[1][0] == '<':
+            temp = eval(settings[1][1:].split(':')[0])
+            split_key = temp
+        else:
+            split_key = eval(settings[1])
+        video_preview_coords = eval(settings[2])
+        blackscreen_threshold = eval(settings[3])
+        max_capture_rate = eval(settings[4])
+        after_split_delay = eval(settings[5])
 
     # Setup
     if setup_at_start:
         # Set Split Key
-        print("Press Split Key")
-        # TODO: implement
+        print("Press the key you use for splitting:")
+        with KeyboardListener(on_press=on_press_split_key) as keyboard_listener:
+            keyboard_listener.join()
 
         # Set video preview coords
-        print("Click to set video preview coords (pick 2 diagonally opposed corners)")
+        print("Click to set video preview coords: (pick 2 diagonally opposed corners)")
         with MouseListener(on_click=on_click_set_coords) as mouse_listener:
             mouse_listener.join()
+
+        # Set blackscreen threshold
+        user_input = int(
+            input("Set maximum grey value to still count as black: (0-255, black-white, 0 by default)\n"))
+        if 0 <= user_input <= 255:
+            blackscreen_threshold = user_input
+        else:
+            blackscreen_threshold = 0
+
+        # Set after split delay
+        user_input = int(
+            input("Set minimum delay between splits to prevent repeated splitting on the same blackscreen: (time in seconds)\n"))
+        if user_input > 0:
+            after_split_delay = user_input
+        else:
+            after_split_delay = 10
 
         # Enable/Disable setup on future program starts depending on user input
         print("Setup done!")
         print("Don't forget to enter your splits manually in splits.txt!")
-        input("Skip setup at future program starts? (y/n)")
-        # TODO: implement
+        user_input = input("Skip setup at future program starts? (y/n)\n")
+        if user_input == "y" or user_input == "Y" or user_input == "yes":
+            setup_at_start = False
+        else:
+            setup_at_start = True
 
+        # Write changes to config.cfg
+        with open("config.cfg", 'w') as config_file:
+            config_file.write(repr(setup_at_start) + "\n")
+            config_file.write(repr(split_key) + "\n")
+            config_file.write(repr(video_preview_coords) + "\n")
+            config_file.write(repr(blackscreen_threshold) + "\n")
+            config_file.write(repr(max_capture_rate) + "\n")
+            config_file.write(repr(after_split_delay))
+
+    # Read splits
+    with open("splits.txt", 'r') as splits_file:
+        lines = splits_file.readlines()
+        for line in lines:
+            if line != "":
+                splits.append(int(line))
+
+    # Main loop
     while True:
         start_time = time.time()
 
