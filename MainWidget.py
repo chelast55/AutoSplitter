a6660378e4073bde01474b5a463619d829573732
myluki2000 <lukas636@live.com>
2021-06-28 15:12:43 +0200

    Improve MainWidget
    Add worker status label and pause button to MainWidget

--- MainWidget.py
+++ MainWidget.py
@@ -3,7 +3,7 @@ from typing import Optional
 from PySide6 import QtCore, QtGui
 from PySide6.QtCore import QThread
 from PySide6.QtGui import QCloseEvent
-from PySide6.QtWidgets import QLabel, QWidget, QPushButton, QVBoxLayout
+from PySide6.QtWidgets import QLabel, QWidget, QPushButton, QVBoxLayout, QHBoxLayout
 
 import SplitsProfile
 from ScreenWatchWorker import ScreenWatchWorker
@@ -14,21 +14,34 @@ class MainWidget(QWidget):
     _worker: Optional[ScreenWatchWorker] = None
 
     _btn_start_stop: QPushButton = None
-    _lbl_selected_profile: QLabel
+    _btn_pause: QPushButton = None
+    _lbl_worker_status: QLabel = None
 
     def __init__(self):
         super().__init__()
 
-        self.text = QLabel("Hello World",
-                           alignment=QtCore.Qt.AlignCenter)
+        self._lbl_worker_status = QLabel("Waiting for you to start the screen watch worker.")
 
         self.layout = QVBoxLayout(self)
-        self.layout.addWidget(self.text)
+        self.layout.addWidget(self._lbl_worker_status)
+
+        buttons_layout = QHBoxLayout()
+        self._btn_pause = QPushButton("Pause")
+        self._btn_pause.clicked.connect(self._btn_pause_on_click)
+        buttons_layout.addWidget(self._btn_pause)
 
         self._btn_start_stop = QPushButton("Start")
-        self.layout.addWidget(self._btn_start_stop)
-
         self._btn_start_stop.clicked.connect(self._btn_start_stop_on_click)
+        buttons_layout.addWidget(self._btn_start_stop)
+        self.layout.addLayout(buttons_layout)
+
+    def _btn_pause_on_click(self):
+        if self._worker.is_paused():
+            self._worker.unpause()
+            self._btn_pause.setText("Pause")
+        else:
+            self._worker.pause()
+            self._btn_pause.setText("Unpause")
 
     def _btn_start_stop_on_click(self):
         # if worker is not started start it, otherwise stop it
@@ -45,6 +58,7 @@ class MainWidget(QWidget):
         self._worker.moveToThread(self._workerThread)
         self._workerThread.started.connect(self._worker.run)
         self._workerThread.start()
+        self._lbl_worker_status.setText("Worker running with profile " + self._worker.get_splits_profile().name + ".")
 
     def _stop_worker(self):
         if self._worker is not None:
@@ -55,6 +69,8 @@ class MainWidget(QWidget):
             self._workerThread.quit()
             self._workerThread.wait()
         self._workerThread = None
+
+        self._lbl_worker_status.setText("Worker stopped.")
 
     def closeEvent(self, event: QCloseEvent):
         self._stop_worker()
--- ScreenWatchWorker.py
+++ ScreenWatchWorker.py
@@ -11,20 +11,33 @@ import Config
 
 class ScreenWatchWorker(QtCore.QObject):
     _finished: bool = False
-    currently_paused: bool = False
-    mouse = MouseController()
-    keyboard = KeyboardController()
-    blackscreen_counter: int = 0
-    reset_after_this_iteration: bool = False
+    _currently_paused: bool = False
+    _mouse = MouseController()
+    _keyboard = KeyboardController()
+    _blackscreen_counter: int = 0
+    _reset_after_this_iteration: bool = False
     _splits_profile: SplitsProfile.SplitsProfile = None
 
     def __init__(self, _splits_profile):
         super(ScreenWatchWorker, self).__init__()
         self._splits_profile = _splits_profile
 
-    @property
-    def splits_profile(self):
+    def get_splits_profile(self):
         return self._splits_profile
+
+    def get_blackscreen_counter(self):
+        return self._blackscreen_counter
+
+    def is_paused(self):
+        return self._currently_paused
+
+    def pause(self):
+        print("Worker paused.")
+        self._currently_paused = True
+
+    def unpause(self):
+        print("Worker unpaused.")
+        self._currently_paused = False
 
     def finish(self):
         self._finished = True
@@ -32,30 +45,30 @@ class ScreenWatchWorker(QtCore.QObject):
     def run(self):
         def on_key_press(key):
             if repr(key) == Config.decrement_key:
-                self.blackscreen_counter -= 1
+                self._blackscreen_counter -= 1
                 print("Blackscreen counter was decremented")
-                print("New Blackscreen Count: " + str(self.blackscreen_counter))
+                print("New Blackscreen Count: " + str(self._blackscreen_counter))
                 time.sleep(Config.after_key_press_delay)
             elif repr(key) == Config.increment_key:
-                self.blackscreen_counter += 1
+                self._blackscreen_counter += 1
                 print("Blackscreen counter was incremented")
-                print("New Blackscreen Count: " + str(self.blackscreen_counter))
+                print("New Blackscreen Count: " + str(self._blackscreen_counter))
                 time.sleep(Config.after_key_press_delay)
             elif repr(key) == Config.reset_key:
-                self.reset_after_this_iteration = True
+                self._reset_after_this_iteration = True
                 print("Reset!")
                 print("Wait for splitter to restart...")
                 time.sleep(Config.after_key_press_delay)
             elif repr(key) == Config.pause_key:
-                if self.currently_paused:
-                    self.currently_paused = False
+                if self._currently_paused:
+                    self._currently_paused = False
                     print("Unpaused!")
                 else:
-                    self.currently_paused = True
+                    self._currently_paused = True
                     print("Paused...")
                 time.sleep(Config.after_key_press_delay)
 
-        print("Starting splitter worker for profile " + self.splits_profile.name)
+        print("Starting splitter worker for profile " + self._splits_profile.name)
 
         # Enable Keys (Decrement, Increment, Reset, Pause)
         key_press_listener = KeyboardListener(on_press=on_key_press)
@@ -64,7 +77,7 @@ class ScreenWatchWorker(QtCore.QObject):
         # Main loop
         # NOTE: this tool is HEAVILY inspired by this video by Code Bullet: https://www.youtube.com/watch?v=wHRubMACen0
         while not self._finished:
-            if not self.currently_paused:
+            if not self._currently_paused:
                 start_time = time.time()
 
                 screen = np.array(ImageGrab.grab(bbox=Config.video_preview_coords))
@@ -72,18 +85,20 @@ class ScreenWatchWorker(QtCore.QObject):
                 print("Average Grey Value: " + str(np.average(screen)))  # Enable for Debug
 
                 if np.average(screen) <= Config.blackscreen_threshold:
-                    self.blackscreen_counter += 1
-                    print("Blackscreen Count: " + str(self.blackscreen_counter))
-                    if self.blackscreen_counter in self._splits_profile.splits:
+                    self._blackscreen_counter += 1
+                    print("Blackscreen Count: " + str(self._blackscreen_counter))
+                    if self._blackscreen_counter in self._splits_profile.splits:
                         print("Pressing " + repr(Config.split_key))
-                        self.keyboard.press(Config.split_key)
+                        self._keyboard.press(Config.split_key)
                     time.sleep(Config.after_split_delay)
 
                     # print("Time per Cycle: " + str(time.time() - start_time)) # Enable for Debug
                     if (time.time() - start_time) < (1 / Config.max_capture_rate):
                         time.sleep((1 / Config.max_capture_rate) - (time.time() - start_time))
 
-            if self.reset_after_this_iteration:
-                self.blackscreen_counter = 0
-                self.reset_after_this_iteration = False
+            if self._reset_after_this_iteration:
+                self._blackscreen_counter = 0
+                self._reset_after_this_iteration = False
                 print("Splitter reset!")
+
+    print("Worker stopped.")
