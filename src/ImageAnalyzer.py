4f49abfc5da2a3c4cefc63ef3d6fcaf76645252e
chelast55 <tim@amoto.de>
2021-07-18 11:54:58 +0200

    Improve documentation for ImageAnalyzer.py

--- src/ImageAnalyzer.py
+++ src/ImageAnalyzer.py
@@ -1,9 +1,21 @@
+"""
+Contains method(s) for analyzing images.
+"""
+
 import cv2
 import numpy as np
 from PIL.Image import Image
 
 
 def average_gray_value(img: Image):
+    """
+    Calculates average gray value of all pixels of a (color) image.
+
+    For this prupose, the image is first converted to grayscale.
+
+    :param img: (Image) color image
+    :return: (double) average gray value
+    """
     screen = np.array(img)
     screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
     return np.average(screen)
