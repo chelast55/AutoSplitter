cdbc7635a9f95431175c5e628456c6d2eae14878
chelast55 <tim@amoto.de>
2021-10-08 23:42:39 +0200

    Add switch for toggling between global and per-profile settings modes and improve info hitboxes (per-profile settings can not be utilized yet))

--- src/SetupWidget.py
+++ src/SetupWidget.py
@@ -1,6 +1,7 @@
 """(GUI) Graphical Settings Menu for setting coordinates for area to observe and various other config parameters"""
 
 import math
+import os
 
 from PIL.Image import Image
 from PySide6.QtCore import Qt, QTimer, QThread
@@ -24,7 +25,13 @@ class SetupWidget(QWidget):
         self.setAttribute(Qt.WA_AlwaysShowToolTips, True)
 
         self.setWindowTitle("Settings")
-        self.resize(1400, 600)
+        self.resize(1400, 700)
+
+        self._global_options_mode_enabled: bool = True
+        """
+        True: Global settings change when edited\n
+        False: Override of changed settings for current splits profile is created
+        """
 
         self._tmr_preview_image: QTimer = QTimer(self)
         self._tmr_preview_image.setInterval(200)
@@ -44,21 +51,31 @@ class SetupWidget(QWidget):
 
         self._gv_preview_image: QRectSelectGraphicsView = QRectSelectGraphicsView()
         self._btn_box: QDialogButtonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        self._btn_change_options_mode: QPushButton = QPushButton("Edit Per-Profile Settings")
         self._btn_restore_defaults: QPushButton = QPushButton("Restore Default Settings")
+        self._lbl_options_mode_status: QLabel = QLabel("Global settings:")
+        self._lbl_split: QLabel = QLabel("Split Key:")
         self._key_picker_split: KeyPickerWidget = KeyPickerWidget()
+        self._lbl_pause: QLabel = QLabel("Pause Key:")
         self._key_picker_pause: KeyPickerWidget = KeyPickerWidget()
+        self._lbl_reset: QLabel = QLabel("Reset Key:")
         self._key_picker_reset: KeyPickerWidget = KeyPickerWidget()
+        self._lbl_decrement: QLabel = QLabel("Decrement Key:")
         self._key_picker_decrement: KeyPickerWidget = KeyPickerWidget()
+        self._lbl_increment: QLabel = QLabel("Increment Key:")
         self._key_picker_increment: KeyPickerWidget = KeyPickerWidget()
+        self._lbl_blackscreen_threshold: QLabel = QLabel("Blackscreen Threshold (0-255):")
         self._sb_blackscreen_threshold: QSpinBox = QSpinBox()
-        self._btn_automatic_threshold: QPushButton = QPushButton("Start Automatic Threshold Detection")
-        self._btn_automatic_threshold.setCheckable(True)
         self._sb_blackscreen_threshold.setMinimum(0)
         self._sb_blackscreen_threshold.setMaximum(255)
+        self._btn_automatic_threshold: QPushButton = QPushButton("Start Automatic Threshold Detection")
+        self._btn_automatic_threshold.setCheckable(True)
+        self._lbl_after_split_delay: QLabel = QLabel("After Split Delay (s):")
         self._dsb_after_split_delay: QDoubleSpinBox = QDoubleSpinBox()
         self._dsb_after_split_delay.setDecimals(2)
         self._dsb_after_split_delay.setMinimum(0)
         self._dsb_after_split_delay.setMaximum(999)
+        self._lbl_advanced_settings: QLabel = QLabel("Show Advanced Settings")
         self._cb_advanced_settings: QCheckBox = QCheckBox()
         self._lbl_max_capture_rate: QLabel = QLabel()
         self._sb_max_capture_rate: QSpinBox = QSpinBox()
@@ -103,20 +120,25 @@ class SetupWidget(QWidget):
         self.setWindowModality(Qt.ApplicationModal)
         self.layout = QVBoxLayout(self)
 
+        if Config.path_to_current_splits_profile == "":
+            self._btn_change_options_mode.setEnabled(False)
+
         settings_layout = QHBoxLayout()
 
         settings_and_info_layout = QVBoxLayout()
+        settings_and_info_layout.addWidget(self._btn_change_options_mode)
         settings_and_info_layout.addWidget(self._btn_restore_defaults)
+        settings_and_info_layout.addWidget(self._lbl_options_mode_status)
         button_settings_layout = QFormLayout()
-        button_settings_layout.addRow("Split Key:", self._key_picker_split)
-        button_settings_layout.addRow("Pause Key:", self._key_picker_pause)
-        button_settings_layout.addRow("Reset Key:", self._key_picker_reset)
-        button_settings_layout.addRow("Decrement Key:", self._key_picker_decrement)
-        button_settings_layout.addRow("Increment Key:", self._key_picker_increment)
-        button_settings_layout.addRow("Blackscreen Threshold (0-255):", self._sb_blackscreen_threshold)
+        button_settings_layout.addRow(self._lbl_split, self._key_picker_split)
+        button_settings_layout.addRow(self._lbl_pause, self._key_picker_pause)
+        button_settings_layout.addRow(self._lbl_reset, self._key_picker_reset)
+        button_settings_layout.addRow(self._lbl_decrement, self._key_picker_decrement)
+        button_settings_layout.addRow(self._lbl_increment, self._key_picker_increment)
+        button_settings_layout.addRow(self._lbl_blackscreen_threshold, self._sb_blackscreen_threshold)
         button_settings_layout.addWidget(self._btn_automatic_threshold)
-        button_settings_layout.addRow("After Split Delay (s):", self._dsb_after_split_delay)
-        button_settings_layout.addRow("Show Advanced Settings", self._cb_advanced_settings)
+        button_settings_layout.addRow(self._lbl_after_split_delay, self._dsb_after_split_delay)
+        button_settings_layout.addRow(self._lbl_advanced_settings, self._cb_advanced_settings)
         button_settings_layout.addRow(self._lbl_max_capture_rate, self._sb_max_capture_rate)
         button_settings_layout.addRow(self._lbl_after_key_press_delay, self._dsb_after_key_press_delay)
         button_settings_layout.addRow(self._lbl_automatic_threshold_overhead, self._sb_automatic_threshold_overhead)
@@ -142,6 +164,7 @@ class SetupWidget(QWidget):
 
         self.layout.addLayout(settings_layout)
 
+        self._btn_change_options_mode.clicked.connect(self._btn_switch_options_mode_on_click)
         self._btn_restore_defaults.clicked.connect(self._btn_restore_defaults_on_click)
         self._btn_automatic_threshold.toggled.connect(self._btn_automatic_threshold_on_toggle)
         self._cb_advanced_settings.stateChanged.connect(self._cb_advanced_settings_state_changed)
@@ -151,46 +174,44 @@ class SetupWidget(QWidget):
         self.layout.addWidget(self._btn_box)
 
     def _on_info_timeout(self):
-        if self._btn_restore_defaults.underMouse():
-            self._lbl_info.setText("Restore default settings.\nNOTE: Key bindings are NOT affected by this.")
-        elif self._key_picker_split.get_button().underMouse():
+        if self._btn_change_options_mode.underMouse():
+            if self._global_options_mode_enabled:
+                self._lbl_info.setText("Switch to \"per-profile settings override mode\".\n"
+                                       "Changes are saved on top of global settings for each splits\nprofile individually.")
+            else:
+                self._lbl_info.setText("Switch to \"global settings mode\".\n"
+                                       "Changes are saved to global settings.")
+        elif self._btn_restore_defaults.underMouse():
+            self._lbl_info.setText("Restore default settings.\n(NOTE: Key bindings are NOT affected by this.)")
+        elif self._lbl_split.underMouse() or self._key_picker_split.underMouse():
             self._lbl_info.setText("Key automatically pressed when a blackscreen is detected")
-            self._append_automatic_instructions()
-        elif self._key_picker_pause.get_button().underMouse():
+        elif self._lbl_pause.underMouse() or self._key_picker_pause.underMouse():
             self._lbl_info.setText("Key to pause/unpause the blackscreen detection")
-            self._append_automatic_instructions()
-        elif self._key_picker_reset.get_button().underMouse():
+        elif self._lbl_reset.underMouse() or self._key_picker_reset.underMouse():
             self._lbl_info.setText("Key to reset the blackscreen counter")
-            self._append_automatic_instructions()
-        elif self._key_picker_decrement.get_button().underMouse():
+        elif self._lbl_decrement.underMouse() or self._key_picker_decrement.underMouse():
             self._lbl_info.setText("Key to subtract 1 from the blackscreen counter")
-            self._append_automatic_instructions()
-        elif self._key_picker_increment.get_button().underMouse():
+        elif self._lbl_increment.underMouse() or self._key_picker_increment.underMouse():
             self._lbl_info.setText("Key to add 1 to the blackscreen counter")
-            self._append_automatic_instructions()
         elif self._btn_automatic_threshold.underMouse():
             self._lbl_info.setText("Enter automatic blackscreen detection mode.")
-            self._append_automatic_instructions()
-        elif self._sb_blackscreen_threshold.underMouse():
+        elif self._lbl_blackscreen_threshold.underMouse() or self._sb_blackscreen_threshold.underMouse():
             self._lbl_info.setText("Maximum Avg. Gray Value the selected area can have to still be\nconsidered a "
                                    "\"blackscreen\"")
-            self._append_automatic_instructions()
-        elif self._dsb_after_split_delay.underMouse():
+        elif self._lbl_after_split_delay.underMouse() or self._dsb_after_split_delay.underMouse():
             self._lbl_info.setText("Delay after a blackscreen was successfully detected to\nprevent multiple splits "
                                    "per blackscreen")
-            self._append_automatic_instructions()
-        elif self._cb_advanced_settings.underMouse():
+        elif self._lbl_advanced_settings.underMouse() or self._cb_advanced_settings.underMouse():
             self._lbl_info.setText("The default values for these should work in most cases")
-            self._append_automatic_instructions()
-        elif self._sb_max_capture_rate.underMouse():
-            self._lbl_info.setText("Times/second a capture is taken (NOTE: this is a maximum and possibly unreachable)")
-            self._append_automatic_instructions()
-        elif self._dsb_after_key_press_delay.underMouse():
+        elif self._lbl_max_capture_rate.underMouse() or self._sb_max_capture_rate.underMouse():
+            self._lbl_info.setText("Times/second a capture is taken\n(NOTE: this is a maximum and possibly unreachable)")
+        elif self._lbl_after_key_press_delay.underMouse() or self._dsb_after_key_press_delay.underMouse():
             self._lbl_info.setText("Delay after any key press to prevent multiple registrations")
-            self._append_automatic_instructions()
-        elif self._sb_automatic_threshold_overhead.underMouse():
+        elif self._lbl_automatic_threshold_overhead.underMouse() or self._sb_automatic_threshold_overhead.underMouse():
             self._lbl_info.setText("Value added to automatically calculated threshold for better tolerance")
-            self._append_automatic_instructions()
+        else:
+            self._lbl_info.setText("")
+        self._append_automatic_instructions()
 
     def _append_automatic_instructions(self):
         if self._btn_automatic_threshold.isChecked():
@@ -209,6 +230,21 @@ class SetupWidget(QWidget):
         self._gv_preview_image.set_image(img)
         if self._gv_preview_image.has_area():
             self._video_preview_worker.set_crop_coords(self._gv_preview_image.get_rect())
+
+    def _btn_switch_options_mode_on_click(self):
+        if self._global_options_mode_enabled:  # Switch to "per-profile settings override mode"
+            self._global_options_mode_enabled = False
+            self._btn_change_options_mode.setText("Edit Global Settings")
+            self._lbl_options_mode_status.setText("Settings override for profile \""
+                                                  + os.path.basename(Config.path_to_current_splits_profile)[:-5]
+                                                  + "\":")
+            self._lbl_options_mode_status.setStyleSheet("color: green")
+            # TODO: implement loading end editing of per-profile settings
+        else:  # Switch to "global settings mode"
+            self._global_options_mode_enabled = True
+            self._btn_change_options_mode.setText("Edit Per-Profile Settings")
+            self._lbl_options_mode_status.setText("Global settings:")
+            self._lbl_options_mode_status.setStyleSheet("color: black")
 
     def _btn_restore_defaults_on_click(self):
         Config.restore_defaults()
