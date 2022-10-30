322f85bf6123be00fa96a8911ea28c44cef07eae
chelast55 <tin@amoto.de>
2022-10-30 11:19:35 +0100

    Finish refactoring of SettingsWidget __init__

--- src/ui/SettingsWidget.py
+++ src/ui/SettingsWidget.py
@@ -29,88 +29,60 @@ class SettingsWidget(QWidget):
         False: Override of changed settings for current splits profile is created
         """
 
-        self.setAttribute(Qt.WA_AlwaysShowToolTips, True)
-
+        # Window Setup
         self.setWindowTitle("Settings")
         self.resize(1400, 700)
+        self.setAttribute(Qt.WA_AlwaysShowToolTips, True)
+        self.setWindowModality(Qt.ApplicationModal)
 
         # Layout
-        _settings_layout = QHBoxLayout()
-
+        self._construct_top_elements()
+        _button_settings_layout: QFormLayout = self._construct_button_settings_layout()
+        self._construct_info_elements()
+        _rect_select_layout: QVBoxLayout = self._construct_rect_select_layout()
         self._btn_box: QDialogButtonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
-        self._btn_change_options_mode: QPushButton = QPushButton("Edit Per-Profile Settings")
-        self._btn_restore_defaults: QPushButton = QPushButton("Restore Default Settings")
-        self._lbl_options_mode_status: QLabel = QLabel("Global settings:")
 
-        self._lbl_info: QLabel = QLabel()
-        self._lbl_info.setWordWrap(True)
-        self._info_box: QVBoxLayout = QVBoxLayout()
-        self._gb_info_highlight: QGroupBox = QGroupBox("Info:")
-        self._gb_info_highlight.setLayout(self._info_box)
-        self._info_box.addWidget(self._lbl_info)
-
-        _settings_and_info_layout = QVBoxLayout()
+        _settings_and_info_layout: QVBoxLayout = QVBoxLayout()
         _settings_and_info_layout.addWidget(self._btn_change_options_mode)
         _settings_and_info_layout.addWidget(self._btn_restore_defaults)
         _settings_and_info_layout.addWidget(self._lbl_options_mode_status)
-        _button_settings_layout: QFormLayout = self._construct_button_settings_layout()
-
         _settings_and_info_layout.addLayout(_button_settings_layout)
         _settings_and_info_layout.addStretch()
         _settings_and_info_layout.addWidget(self._gb_info_highlight)
-        _settings_layout.addLayout(_settings_and_info_layout)
 
-        rect_select_layout: QVBoxLayout = self._construct_rect_select_layout()
-        _settings_layout.addLayout(rect_select_layout)
+        _settings_layout: QHBoxLayout = QHBoxLayout()
+        _settings_layout.addLayout(_settings_and_info_layout)
+        _settings_layout.addLayout(_rect_select_layout)
 
         self._layout = QVBoxLayout(self)
         self._layout.addLayout(_settings_layout)
         self._layout.addWidget(self._btn_box)
 
-        # hide advanced settings and disable override spin boxes initially
+        # misc. visual setup
         self._hide_advanced_settings()
         self._disable_override_spin_boxes()
+        self._load_from_config()
 
-        # Video Preview
+        # Background tasks
         self._init_video_preview_thread()
-
-        # Info Tooltip Timer
-        self._tmr_info: QTimer = QTimer(self)
-        self._tmr_info.setInterval(200)
-        # self._tmr_info.timeout.connect(self._on_info_timeout)
-        self._tmr_info.start()
-
-        self._key_picker_split.set_key(config.get_split_key())
-        self._key_picker_pause.set_key(config.get_pause_key())
-        self._key_picker_reset.set_key(config.get_reset_key())
-        self._key_picker_decrement.set_key(config.get_decrement_key())
-        self._key_picker_increment.set_key(config.get_increment_key())
-        self._sb_blackscreen_threshold.setValue(config.get_blackscreen_threshold())
-        self._dsb_after_split_delay.setValue(config.get_after_split_delay())
-        self._lbl_max_capture_rate.setText("Max Capture Rate (1/s):")
-        self._sb_max_capture_rate.setValue(config.get_max_capture_rate())
-        self._lbl_after_key_press_delay.setText("After Key Press Delay (s):")
-        self._dsb_after_key_press_delay.setValue(config.get_after_key_press_delay())
-        self._lbl_automatic_threshold_overhead.setText("Automatic Threshold Overhead (0-255):")
-        self._sb_automatic_threshold_overhead.setValue(config.get_automatic_threshold_overhead())
-
-        self.setWindowModality(Qt.ApplicationModal)
-
-        if config.get_current_splits_profile_path() == "":
-            self._btn_change_options_mode.setEnabled(False)
+        self._init_info_timer()
 
         # connect functionality to buttons
         # self._btn_change_options_mode.clicked.connect(self._btn_switch_options_mode_on_click)
         # self._btn_restore_defaults.clicked.connect(self._btn_restore_defaults_on_click)
         # self._btn_automatic_threshold.toggled.connect(self._btn_automatic_threshold_on_toggle)
         # self._cb_advanced_settings.stateChanged.connect(self._cb_advanced_settings_state_changed)
-
         # self._btn_box.accepted.connect(self._btn_box_accepted)
         # self._btn_box.rejected.connect(self._btn_box_rejected)
 
     #########################
     # Construct sub-layouts #
     #########################
+
+    def _construct_top_elements(self) -> None:
+        self._btn_change_options_mode: QPushButton = QPushButton("Edit Per-Profile Settings")
+        self._btn_restore_defaults: QPushButton = QPushButton("Restore Default Settings")
+        self._lbl_options_mode_status: QLabel = QLabel("Global settings:")
 
     def _construct_button_settings_layout(self) -> QFormLayout:
         # Initialize
@@ -133,13 +105,13 @@ class SettingsWidget(QWidget):
         self._dsb_after_split_delay_override: QDoubleSpinBox = QDoubleSpinBox()
         self._lbl_advanced_settings: QLabel = QLabel("Show Advanced Settings")
         self._cb_advanced_settings: QCheckBox = QCheckBox()
-        self._lbl_max_capture_rate: QLabel = QLabel()
+        self._lbl_max_capture_rate: QLabel = QLabel("Max Capture Rate (1/s):")
         self._sb_max_capture_rate: QSpinBox = QSpinBox()
         self._sb_max_capture_rate_override: QSpinBox = QSpinBox()
-        self._lbl_after_key_press_delay: QLabel = QLabel()
+        self._lbl_after_key_press_delay: QLabel = QLabel("After Key Press Delay (s):")
         self._dsb_after_key_press_delay: QDoubleSpinBox = QDoubleSpinBox()
         self._dsb_after_key_press_delay_override: QDoubleSpinBox = QDoubleSpinBox()
-        self._lbl_automatic_threshold_overhead: QLabel = QLabel()
+        self._lbl_automatic_threshold_overhead: QLabel = QLabel("Automatic Threshold Overhead (0-255):")
         self._sb_automatic_threshold_overhead: QSpinBox = QSpinBox()
         self._sb_automatic_threshold_overhead_override: QSpinBox = QSpinBox()
 
@@ -196,6 +168,16 @@ class SettingsWidget(QWidget):
                                                       self._sb_automatic_threshold_overhead_override))
         return button_settings_layout
 
+    def _construct_info_elements(self) -> None:
+        self._lbl_info: QLabel = QLabel()
+        self._lbl_info.setWordWrap(True)
+
+        self._info_box: QVBoxLayout = QVBoxLayout()
+        self._info_box.addWidget(self._lbl_info)
+
+        self._gb_info_highlight: QGroupBox = QGroupBox("Info:")
+        self._gb_info_highlight.setLayout(self._info_box)
+
     def _construct_rect_select_layout(self) -> QVBoxLayout:
         # Initialize
         self._lbl_gray_value: QLabel = QLabel("Avg. Gray Value: -")
@@ -233,6 +215,26 @@ class SettingsWidget(QWidget):
         # self._video_preview_worker.image_captured.connect(self._preview_on_image_captured)
         self._video_preview_thread.start()
 
+    def _init_info_timer(self):
+        self._tmr_info: QTimer = QTimer(self)
+        self._tmr_info.setInterval(200)
+        # self._tmr_info.timeout.connect(self._on_info_timeout)
+        self._tmr_info.start()
+
+    def _load_from_config(self): # TODO: support for overrides
+        if config.get_current_splits_profile_path() == "":
+            self._btn_change_options_mode.setEnabled(False)
+        self._key_picker_split.set_key(config.get_split_key())
+        self._key_picker_pause.set_key(config.get_pause_key())
+        self._key_picker_reset.set_key(config.get_reset_key())
+        self._key_picker_decrement.set_key(config.get_decrement_key())
+        self._key_picker_increment.set_key(config.get_increment_key())
+        self._sb_blackscreen_threshold.setValue(config.get_blackscreen_threshold())
+        self._dsb_after_split_delay.setValue(config.get_after_split_delay())
+        self._sb_max_capture_rate.setValue(config.get_max_capture_rate())
+        self._dsb_after_key_press_delay.setValue(config.get_after_key_press_delay())
+        self._sb_automatic_threshold_overhead.setValue(config.get_automatic_threshold_overhead())
+
     def _hide_advanced_settings(self):
         self._lbl_max_capture_rate.setVisible(False)
         self._sb_max_capture_rate.setVisible(False)
@@ -261,3 +263,10 @@ class SettingsWidget(QWidget):
         self._sb_max_capture_rate_override.setEnabled(False)
         self._dsb_after_key_press_delay_override.setEnabled(False)
         self._sb_automatic_threshold_overhead_override.setEnabled(False)
+
+    def _enable_override_spin_boxes(self):
+        self._sb_blackscreen_threshold_override.setEnabled(True)
+        self._dsb_after_split_delay_override.setEnabled(True)
+        self._sb_max_capture_rate_override.setEnabled(True)
+        self._dsb_after_key_press_delay_override.setEnabled(True)
+        self._sb_automatic_threshold_overhead_override.setEnabled(True)
